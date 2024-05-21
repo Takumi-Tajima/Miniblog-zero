906b32bb4276a0dc16cbd7fc89a2cdedc368805a
taji <taji@sonicgarden.jp>
2024-05-21 20:13:14 +0900

    nameのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true, format: { with: /\A[a-zA-Z0-9]+\z/ }, length: { maximum: 20 }
 end
