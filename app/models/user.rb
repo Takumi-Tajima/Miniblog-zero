1f76f0678818dea08a44a0b3cfd45159b4b917ca
taji <taji@sonicgarden.jp>
2024-05-22 10:10:09 +0900

    名前にアンダースコアを許容する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true, format: { with: /\A[a-zA-Z0-9]+\z/ }, length: { maximum: 20 }
+  validates :name, presence: true, format: { with: /\A\w+\z/ }, length: { maximum: 20 }
 end
