5472bd4089b6038daee0a310b828aef7b773d51b
taji <taji@sonicgarden.jp>
2024-05-22 15:27:46 +0900

    postsテーブルをマイグレート

--- db/migrate/20240522062531_create_posts.rb
+++ db/migrate/20240522062531_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
-      t.text :content
+      t.text :content, null: false, default: ''
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
