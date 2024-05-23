7710afe322f58ddaae6755d7b11f97968fdb465d
taji <taji@sonicgarden.jp>
2024-05-23 10:24:42 +0900

    user.rbにhas_many :postを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :posts, dependent: :destroy
   validates :name, presence: true, format: { with: /\A\w+\z/ }, length: { maximum: 20 }
 end
