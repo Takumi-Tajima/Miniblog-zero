6fdb9bf54dedf51a7cbd7ea4b4b117d875d4e56c
taji <taji@sonicgarden.jp>
2024-05-23 10:28:15 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'posts#index'
   devise_for :users
+  scope module: :usre do
+    resources :posts, only: %i[new create edit update destory]
+  end
+  resources :posts, only: %i[index show]
 end
