3314c1401d86c704817ce0c40fcc31c39d7c86e1
taji <taji@sonicgarden.jp>
2024-05-24 16:59:19 +0900

    routesのタイポを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index'
   devise_for :users
-  scope module: :usre do
+  scope module: :users do
     resources :posts, only: %i[new create edit update destory]
   end
   resources :posts, only: %i[index show]
