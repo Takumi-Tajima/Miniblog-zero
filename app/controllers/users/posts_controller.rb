619c77aca16de9caeec1e6f788d1a530b44201bd
taji <taji@sonicgarden.jp>
2024-05-26 21:23:46 +0900

    ストロングパタメータからuser_idの許容を削除

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -37,6 +37,6 @@ class Users::PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:content, :user_id)
+    params.require(:post).permit(:content)
   end
 end
