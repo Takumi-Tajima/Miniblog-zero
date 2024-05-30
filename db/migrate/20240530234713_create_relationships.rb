52a49d06964f2dfc2d56d30b0968f47cfdb2e69c
taji <taji@sonicgarden.jp>
2024-05-31 08:49:25 +0900

    relationshipsテーブルに設定を追加

--- db/migrate/20240530234713_create_relationships.rb
+++ db/migrate/20240530234713_create_relationships.rb
@@ -1,10 +1,11 @@
 class CreateRelationships < ActiveRecord::Migration[7.1]
   def change
     create_table :relationships do |t|
-      t.integer :follower_id
-      t.integer :followed_id
+      t.integer :follower_id, null: false
+      t.integer :followed_id, null: false
 
       t.timestamps
+      t.index %i[follower_id followed_id], unique: true
     end
   end
 end
