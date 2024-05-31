6ba4180086c375946853ed03cf85ca395ac0f9b6
taji <taji@sonicgarden.jp>
2024-05-31 09:58:40 +0900

    user.rbにfollowingsでフォローしているユーザーをとってくるように定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
   has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy, inverse_of: 'follower_id'
+  has_many :followings, through: :active_relationships, source: followed
   validates :name, presence: true, format: { with: /\A\w+\z/ }, length: { maximum: 20 }
 end
