64d28eb23c991d57297b29ce20feaa50575b7dfc
taji <taji@sonicgarden.jp>
2024-05-31 09:48:49 +0900

    user.rb：has_many :active_relationships追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy, inverse_of: 'follower_id'
   validates :name, presence: true, format: { with: /\A\w+\z/ }, length: { maximum: 20 }
 end
