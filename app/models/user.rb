cf119ddc7e5b151a6fe62fe105b5bbd50f302fed
taji <taji@sonicgarden.jp>
2024-05-31 10:16:34 +0900

    user.rbのタイポ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
-  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy, inverse_of: 'follower_id'
-  has_many :followings, through: :active_relationships, source: followed
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy, inverse_of: 'follower'
+  has_many :followings, through: :active_relationships, source: :followed
   validates :name, presence: true, format: { with: /\A\w+\z/ }, length: { maximum: 20 }
 
   def follow(user)
