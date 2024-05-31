d7db90b18a1d1b480a874651b7edc61d7268cb1e
taji <taji@sonicgarden.jp>
2024-05-31 09:59:42 +0900

    フォロー、フォロー解除メソッドを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy, inverse_of: 'follower_id'
   has_many :followings, through: :active_relationships, source: followed
   validates :name, presence: true, format: { with: /\A\w+\z/ }, length: { maximum: 20 }
+
+  def follow(user)
+    active_relationships.create(followed_id: user.id)
+  end
+
+  def unfollow!(user)
+    active_relationships.find(followed_id: user.id).destroy!
+  end
 end
