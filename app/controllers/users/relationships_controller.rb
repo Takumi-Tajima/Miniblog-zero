6630f0f33e3e1f1afb0363ce6b6e091b4bfe9cd1
taji <taji@sonicgarden.jp>
2024-06-02 11:46:07 +0900

    relationships_controllerにアクションを追加

--- app/controllers/users/relationships_controller.rb
+++ app/controllers/users/relationships_controller.rb
@@ -1,7 +1,11 @@
 class Users::RelationshipsController < ApplicationController
   def create
+    current_user.follow(params[:user])
+    redirect_to request.referer || root_path
   end
 
   def destroy
+    current_user.unfollow!(params[:user])
+    redirect_to request.referer || root_path
   end
 end
