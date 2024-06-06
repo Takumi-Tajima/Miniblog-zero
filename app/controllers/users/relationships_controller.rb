fab775ab1578907b41d102cfed17332850685817
taji <taji@sonicgarden.jp>
2024-06-06 17:15:22 +0900

    paramsから受け取る値をuser_idに変更する

--- app/controllers/users/relationships_controller.rb
+++ app/controllers/users/relationships_controller.rb
@@ -1,11 +1,11 @@
 class Users::RelationshipsController < ApplicationController
   def create
-    current_user.follow(params[:user])
+    current_user.follow(params[:user_id])
     redirect_to request.referer || root_path
   end
 
   def destroy
-    current_user.unfollow!(params[:user])
+    current_user.unfollow!(params[:user_id])
     redirect_to request.referer || root_path
   end
 end
