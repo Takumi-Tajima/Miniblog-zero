797515010fa4ad26a6420420acbb5654ad67a32f
taji <taji@sonicgarden.jp>
2024-06-07 08:48:19 +0900

    ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   scope module: :users do
     resources :posts, only: %i[new create edit update destroy]
     resource :relationships, only: %i[create destroy]
+    get '/followings_posts', to: 'following_posts#index'
   end
   resources :posts, only: %i[index show]
 end
