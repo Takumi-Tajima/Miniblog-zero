a693f8115f05949fde99f80a0f9c06da36284f32
taji <taji@sonicgarden.jp>
2024-07-05 14:06:50 +0900

    fix: バリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
-  validates :name, presence: true, format: { with: /\A\w+\z/ }, length: { maximum: 20 }
+  validates :name, presence: true, format: { with: /\A[\w_.-]+\z/, message: 'の入力形式が違います' }, length: { maximum: 20 }
 end
