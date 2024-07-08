687b24c34cc6e7375f3101e9f318c6200ab7ac67
taji <taji@sonicgarden.jp>
2024-07-08 13:47:05 +0900

    relationships ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   scope module: :users do
     resources :posts, only: %i[new create edit update destroy]
+    resources :relationships, only: %i[create destroy]
   end
   resources :posts, only: %i[index show]
 end
