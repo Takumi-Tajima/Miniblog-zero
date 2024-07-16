8dee08c3f5c28b87c64997d8bbf963c9ce6808d1
taji <taji@sonicgarden.jp>
2024-07-16 15:12:28 +0900

    リファクタ

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
   scope :default_order, -> { order(updated_at: :desc) }
+
+  def liked_by?(user)
+    likes.any? { |like| like.user_id == user.id }
+  end
 end
